9146fc4c98b4b7fba3c0c44d516a2f412e8486ee
Pavol Gressa <pavol.gressa@oracle.com>
2021-03-01 21:47:48 +0100

    initial commit s3 client

---
+++ src/main/java/micronaut/example/Result.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2017-2020 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package micronaut.example;
+
+import io.micronaut.core.annotation.Introspected;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Introspected
+public class Result {
+
+    private final String requestId;
+
+    @NotNull
+    @NotBlank
+    private final String status;
+
+    public Result(String requestId, @NotNull @NotBlank String status) {
+        this.requestId = requestId;
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getRequestId() {
+        return requestId;
+    }
+}
