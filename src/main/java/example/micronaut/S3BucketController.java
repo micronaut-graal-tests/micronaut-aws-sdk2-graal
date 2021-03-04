7b3faf3b95efc302b49048c986bb53893da4a2cf
Iván López <lopezi@objectcomputing.com>
2021-03-04 17:26:44 +0100

    Use constructor injection

--- src/main/java/example/micronaut/S3BucketController.java
+++ src/main/java/example/micronaut/S3BucketController.java
@@ -16,28 +16,32 @@
 package example.micronaut;
 
 import io.micronaut.context.annotation.Parameter;
-import io.micronaut.core.annotation.ReflectiveAccess;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
 import software.amazon.awssdk.core.exception.SdkException;
 import software.amazon.awssdk.regions.providers.AwsRegionProviderChain;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.*;
+import software.amazon.awssdk.services.s3.model.Bucket;
+import software.amazon.awssdk.services.s3.model.CreateBucketConfiguration;
+import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+import software.amazon.awssdk.services.s3.model.CreateBucketResponse;
+import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
+import software.amazon.awssdk.services.s3.model.S3Exception;
 
-import javax.inject.Inject;
 import java.util.stream.Collectors;
 
 @Controller("/s3/buckets")
 public class S3BucketController {
 
-    @ReflectiveAccess
-    @Inject
-    private AwsRegionProviderChain awsRegionProviderChain;
+    private final AwsRegionProviderChain awsRegionProviderChain;
+    private final S3Client s3Client;
 
-    @ReflectiveAccess
-    @Inject
-    private S3Client s3Client;
+    public S3BucketController(AwsRegionProviderChain awsRegionProviderChain,
+                              S3Client s3Client) {
+        this.awsRegionProviderChain = awsRegionProviderChain;
+        this.s3Client = s3Client;
+    }
 
     @Post("/{bucketName}")
     public Result createBucket(@Parameter String bucketName) {
@@ -51,7 +55,8 @@ public class S3BucketController {
             CreateBucketResponse response = s3Client.createBucket(createBucketRequest);
             return new Result(response.responseMetadata().requestId(),
                     String.valueOf(response.sdkHttpResponse().statusCode()),
-                    response.location());
+                    response.location()
+            );
         } catch (S3Exception s3Exception) {
             return new Result(s3Exception.requestId(), String.valueOf(s3Exception.statusCode()), s3Exception.getMessage());
         } catch (SdkException sdkException) {
@@ -66,7 +71,8 @@ public class S3BucketController {
             return new ListBucketsResult(
                     response.responseMetadata().requestId(),
                     String.valueOf(response.sdkHttpResponse().statusCode()),
-                    response.buckets().stream().map(Bucket::name).collect(Collectors.toList()));
+                    response.buckets().stream().map(Bucket::name).collect(Collectors.toList())
+            );
         } catch (S3Exception s3Exception) {
             return new ListBucketsResult(s3Exception.requestId(), String.valueOf(s3Exception.statusCode()), null);
         } catch (SdkException sdkException) {
