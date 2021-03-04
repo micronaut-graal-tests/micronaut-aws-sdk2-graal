333b9c21aa31a5af426e54ebb9616a3e69034e27
Iván López <lopezi@objectcomputing.com>
2021-03-04 17:37:13 +0100

    Cleanup

--- src/main/java/example/micronaut/ParamaterStoreController.java
+++ src/main/java/example/micronaut/ParamaterStoreController.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package micronaut.example;
+package example.micronaut;
 
 import io.micronaut.core.annotation.ReflectiveAccess;
 import io.micronaut.http.MediaType;
